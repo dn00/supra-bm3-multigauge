d3f739e01228211f3bfd311244ff52479c64bef6
Dennis <dtnguy90@gmail.com>
2024-01-13 16:22:08 -0800

    changed to file

--- main.py
+++ main.py
@@ -1365,7 +1365,7 @@ class MainApp(MDApp):
         
     def start_bm3_agent(self):
         # Change the current working directory to the script's directory
-        script_directory = os.path.dirname(os.path.abspath(__name__))
+        script_directory = os.path.dirname(os.path.abspath(__file__))
         os.chdir(script_directory)
 
         # Attempt to run the file
